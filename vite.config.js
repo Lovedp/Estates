9b2a5bb067bf8b1d6f0a43b3056d1d0694b5ec4d
Lovedp <ifedolapoemmanuel98@gmail.com>
2025-04-29 16:31:46 +0100

    all

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://estates-2.onrender.com', // Your Express backend
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove '/api' prefix
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false,
       },
     },
   },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        // Node.js modules (backend-only)
-        'mongoose',
-        'bcryptjs',
-        'express',
-        'crypto',       // Common Node.js modules
-        'fs',           // File system (Node-only)
-        'path',         // Node.js path module
-        'http',         // Node.js HTTP module
-        'querystring',   // Node.js querystring module
-      ],
-    },
-  },
 });
