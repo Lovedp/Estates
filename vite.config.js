f02a97e8f18a0abe9f1fd7c2f0b8c2688839f511
Lovedp <ifedolapoemmanuel98@gmail.com>
2025-04-29 16:01:51 +0100

    finals

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Your Express backend
+        target: 'https://estates-2.onrender.com', // Your Express backend
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove '/api' prefix
